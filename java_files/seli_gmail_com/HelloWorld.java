ba24eea8b7b78f27cf667885e5fc56ca5bff2e5a
fransiskalidya <fransiskaly@gmail.com>
2023-02-07 15:35:52 +0700

    new requirement

---
+++ java_files/seli_gmail_com/HelloWorld.java
@@ -0,0 +1,7 @@
+package seli_gmail_com; 
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!"); 
+    }
+}
---
+++ java_files/seli_gmail_com/JUnitHelloWorldTest.java
@@ -0,0 +1,29 @@
+package seli_gmail_com;
+
+import static org.junit.Assert.assertEquals;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JUnitHelloWorldTest {
+  private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+  private final PrintStream oPrintStream = System.out;
+
+  @Before
+  public void setUpStream() {
+    System.setOut(new PrintStream(outputStream));
+  }
+
+  @After
+  public void restoreStream() {
+    System.setOut(oPrintStream);
+  }
+
+  @Test
+  public void isHelloWorld() {
+    seli_gmail_com.HelloWorld.main(null);
+    assertEquals("Is Hello World not same", "Hello World!", outputStream.toString());
+  }
+}
